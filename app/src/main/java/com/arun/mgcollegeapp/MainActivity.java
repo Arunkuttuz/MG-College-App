9a620c55f7f3037128d45dec022e33fde816244c
Arunkuttuz <arunrajmurikkan@gmail.com>
2019-12-05 23:18:57 +0530

    success

--- app/src/main/java/com/arun/mgcollegeapp/MainActivity.java
+++ app/src/main/java/com/arun/mgcollegeapp/MainActivity.java
@@ -3,12 +3,28 @@ package com.arun.mgcollegeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText e1,e2;
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        e1=(EditText)findViewById(R.id.userid);
+        e2=(EditText)findViewById(R.id.password);
+        b1=(Button)findViewById(R.id.btnid);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"success",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
